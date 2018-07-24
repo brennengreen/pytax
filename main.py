5a9a5aa92445605ef1bfc7083ef7b19aa1485911
brennengreen <brennengreen@outlook.com>
2018-07-24 09:37:19 -0400

    added website

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import profile as us
 import taxmath as tmath
 
-user_state = ""
-user_city = ""
-user_zip = ""
+user_state = "Kentucky"
+user_city = "Georgetown"
+user_zip = "40324"
+sale_price = 19.95
 
+'''
+COMMENTED FOR TESTING PURPOSES, UNCOMMENT IF PUSHING TO REPO
 # Using while loops to continuously prompt the user for input in the case they just hit enter/return
 while len(user_state) < 1:
     user_state = input("Please provide a valid state: ")
@@ -12,6 +15,9 @@ while len(user_city) < 1:
     user_city = input("Please provide a valid city: ")
 while len(user_zip) < 1:
     user_zip = input("Please provide a valid zip code: ")
+while sale_price <= 0:
+    sale_price = input("How much does the item cost: ")
+'''
 
 # In order to use the profiles created by profile.py you must create a user profile
 # TO DO: Create profile based on whether or not the sale was made to a US or international customer
@@ -20,7 +26,7 @@ user_profile = us.TaxProfile(user_city, user_state, user_zip)
 
 # Example of the main purpose of taxcalculator, find the total sale cost including all tax rates
 total_sale = round(tmath.calc_total_sale(user_profile.get_rate().combined_rate,
-                                         float(input("How much is the item being sold? "))), 2)
+                                         float(sale_price)), 2)
 
 # A contrary to the intended purpose of this program, you may also use remove_tax ala taxmath to remove the total
 # sales tax from a total sale cost, just in case you needed the original price
